63c3c404096ff2422fad4a409db93e7b632f022f
Mwaki Harri Magotswi <magotswi@gmail.com>
2021-12-23 01:04:46 -0500

    Allow for graceful (and hopefully easy to read) failures

--- lib/transaction_desk.rb
+++ lib/transaction_desk.rb
@@ -2,4 +2,6 @@ require 'transaction_desk/version'
 require 'transaction_desk/configuration'
 
 module TransactionDesk
+  Error = Class.new(StandardError)
+  UnauthorizedError = Class.new(TransactionDesk::Error)
 end
