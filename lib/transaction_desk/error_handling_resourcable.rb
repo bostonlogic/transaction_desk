bf895452422001ba587bae7134a3ae25711478f0
Mwaki Harri Magotswi <magotswi@gmail.com>
2022-04-28 16:55:57 -0400

    Tossing in a couple more error types coz they exist

--- lib/transaction_desk/error_handling_resourcable.rb
+++ lib/transaction_desk/error_handling_resourcable.rb
@@ -4,8 +4,12 @@ module ErrorHandlingResourcable
       default_handler do |response|
         if (200...299).include?(response.status)
           next
+        elsif response.status == 400
+          raise TransactionDesk::UnsupportedApiVersionError.new("#{response.status}: #{response.body}")
         elsif response.status == 401
           raise TransactionDesk::UnauthorizedError.new("#{response.status}: #{response.body}")
+        elsif response.status == 404
+          raise TransactionDesk::NotFoundError.new("#{response.status}: #{response.body}")
         else
           raise TransactionDesk::Error.new("#{response.status}: #{response.body}")
         end
